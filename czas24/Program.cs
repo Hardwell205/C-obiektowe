7e443cc95489b353e25918fa9d18551447b19ea9
Szymon Gliwa <szymon.gliwa@microsoft.wsei.edu.pl>
2019-10-21 16:33:20 +0200

    XD

--- czas24/Program.cs
+++ czas24/Program.cs
@@ -1,15 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace czas24
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string[] napis = null;
+        Czas24h t = null;
+
+        // wczytanie i parsowanie napisu oznaczającego godzinę, np. 2:15:27
+        napis = Console.ReadLine().Split(':');
+        int[] czas = Array.ConvertAll(napis, int.Parse);
+        try
         {
+            t = new Czas24h(czas[0], czas[1], czas[2]);
         }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("error");
+            return;
+        }
+
+        // wczytanie liczby poleceń
+        int liczbaPolecen = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= liczbaPolecen; i++)
+        {
+            // wczytanie polecenia
+            napis = Console.ReadLine().Split(' ');
+            string typPolecenia = napis[0];
+            int liczba = int.Parse(napis[1]);
+
+            // wykonanie polecenia na obiekcie t typu Czas24h
+            try
+            {
+                switch (typPolecenia)
+                {
+                    case "g":
+                        t.Godzina = liczba;
+                        break;
+                    case "m":
+                        t.Minuta = liczba;
+                        break;
+                    case "s":
+                        t.Sekunda = liczba;
+                        break;
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+        }
+        Console.WriteLine(t);
     }
 }
+
+public class Czas24h
+{
+    private int liczbaSekund;
+
+    public int Sekunda
+    {
+        get => liczbaSekund - Godzina * 60 * 60 - Minuta * 60;
+        // uzupełnij kod - zdefiniuj setters'a
+        set
+        {
+            if (value < 0 || value > 60)
+                throw new ArgumentException("error");
+            liczbaSekund = value + 60 * Minuta + 3600 * Godzina;
+        } 
+        
+    }
+
+    public int Minuta
+    {
+        get => (liczbaSekund / 60) % 60;
+        // uzupełnij kod - zdefiniuj setters'a
+        set {
+            if (value < 0 || value > 60)
+                throw new ArgumentException("error");
+            liczbaSekund = Sekunda + 60 * value + 3600 * Godzina;
+        }
+    }
+
+    public int Godzina
+    {
+        get => liczbaSekund / 3600;
+        // uzupełnij kod - zdefiniuj setters'a
+        set {
+            if (value < 0 || value > 24)
+                throw new ArgumentException("error");
+            liczbaSekund = Sekunda + 60 * Minuta + 3600 * value;
+        }
+    }
+
+    public Czas24h(int godzina, int minuta, int sekunda)
+    {
+        // uzupełnij kod zgłaszając wyjątek ArgumentException
+        // w sytuacji niepoprawnych danych
+
+        liczbaSekund = sekunda + 60 * minuta + 3600 * godzina;
+        if (godzina < 0 || godzina > 24)
+        {
+            throw new ArgumentException("error");
+        }
+        if (sekunda < 0 || sekunda > 60)
+        {
+            throw new ArgumentException("error");
+        }
+        if (minuta < 0 || minuta > 60)
+        {
+            throw new ArgumentException("error");
+        }
+    }
+
+    public override string ToString() => $"{Godzina}:{Minuta:D2}:{Sekunda:D2}";
+}
+XDDDDDDDDDDDD
